6b2595de369d85f01a948ae13992a6a5a9e414ba
gorF-iwnl <aaronboziel@gmail.com>
2025-05-11 15:41:11 -0700

    Add files via upload
    Basic number generator for duoleveling project.

---
+++ number_adder.py
@@ -0,0 +1,11 @@
+def add_two_integers(num1, num2):
+  """
+  This function takes two integers and returns their sum.
+  """
+  return num1 + num2
+
+# Example usage:
+number1 = 14
+number2 = 25
+sum_result = add_two_integers(number1, number2)
+print(f"the sum of {number1} and {number2} is: {sum_result}")
