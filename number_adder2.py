3759f8deed3f466b812eb5a44e73324dcf69c0bf
Aaron Boziel <aaronboziel@gmail.com>
2025-05-17 12:54:57 -0700

    Added less than or equal to function

--- number_adder2.py
+++ number_adder2.py
@@ -1,20 +1,20 @@
 # Get input from the user
 input1 = input("Enter the first integer: ")
 input2 = input("Enter the second integer: ")
-input3 = input("Enter the Third Integer:")
+
 
 # Convert the input strings to integers
 # Use a try-except block to handle potential errors if the user enters non-interface
 try:
     num1 = int(input1)
     num2 = int(input2)
-    num3 = int(input3)
-
-    #add the two integers
-    sum_result = num1 + num2 + num3
-
-    # Print the result
-    print(f"The sum of {num1} , {num2} and {num3} is: {sum_result}")
+    
+    if num1 > num2:
+        print(f"The first integer is larger than the second")
+    elif num1 == num2:
+        print(f"The first integer is equal to the second")
+    else:
+        print(f"The first integer is smaller than the second")
 
 except ValueError:
     print(f"Invalid input. Please enter only integers.")
